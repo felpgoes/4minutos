b2b4ac003e9ad7327167db4aa3838be5a0f6ce2e
felpgoes <felipe.goess@hotmail.com>
2023-02-02 23:13:38 -0300

    feat: adding replace to videos

--- 4min.js
+++ 4min.js
@@ -1,59 +1,98 @@
-(function () {
-    //fourMinutes 
-    var main = function ($) {
 
-        var self = $.fourMinutes = new function () { };
 
-        $.extend(self, {
-            fourMinutesImgs: [
-                'https://img.r7.com/images/croacia-x-brasil-copa-do-mundo-09122022145122740?dimensions=771x420&no_crop=true',
-                'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt7d847935bd3ad1ed/639351f602b9015c9dff3421/GettyImages-1447951636.jpg',
-                'https://img.r7.com/images/croacia-x-brasil-copa-do-mundo-09122022145013908?dimensions=771x420&&amp;&amp;resize=771x420&amp;crop=1280x697+0+101&amp;&amp;resize=771x420&amp;crop=1280x697+0+101',
-                'https://www.oitomeia.com.br/wp-content/uploads/2022/12/Brasil-4.jpg',
-                'https://s2.glbimg.com/-VvRCkB2g6Mhd-Cl9M0CQgMXrEg=/0x0:1599x897/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/B/w/WZCfQwTUeRjfMEjP871Q/gol8j.jpg',
-                'https://f.i.uol.com.br/fotografia/2022/12/10/16706922006394bd685b1fd_1670692200_3x2_md.jpg',
-                'https://www.otempo.com.br/image/contentid/policy:1.2780617:1670703082/croacia-x-brasil-jpg.jpg',
-                'https://www.redebrasilatual.com.br/wp-content/uploads/2022/12/BrasilxCroacia_Fifa.jpg',
-                'https://static.dw.com/image/64051338_1006.jpg',
-                'http://m.lance.com.br/files/admin_slider_thumbnail/uploads/2022/12/09/6393768019a14.jpeg',
-            ],
-            handleImages: function (lstImgs, time) {
-                $.each($('img'), function (i, item) {
-                    //Skip if image is already replaced
-                    if ($.inArray($(item).attr('src'), lstImgs) == -1) {
-                        var h = $(item).height();
-                        var w = $(item).width();
-
-                        //If image loaded
-                        if (h > 0 && w > 0) {
-                            //Replace
+(() => {
+    const main = function ($) {
+        
+        const replaceImages = (lstImgs, item) => {
+            if ($.inArray($(item).attr('src'), lstImgs) == -1) {
+                var h = $(item).height();
+                var w = $(item).width();
+        
+                //If image loaded
+                if (h > 0 && w > 0) {
+                    //Replace
+                    $(item).css('width', w + 'px').css('height', h + 'px');
+                    $(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]);
+                    $(item).attr('srcset', lstImgs[Math.floor(Math.random() * lstImgs.length)]);
+        
+                }
+                else {
+                    //Replace when loaded
+                    $(item).load(function () {
+                        //Prevent 'infinite' loop
+                        if ($.inArray($(item).attr('src'), lstImgs) == -1) {
+                            var h = $(item).height();
+                            var w = $(item).width();
                             $(item).css('width', w + 'px').css('height', h + 'px');
                             $(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]);
+                            $(item).attr('srcset', lstImgs[Math.floor(Math.random() * lstImgs.length)]);
+        
                         }
-                        else {
-                            //Replace when loaded
-                            $(item).load(function () {
-                                //Prevent 'infinite' loop
-                                if ($.inArray($(item).attr('src'), lstImgs) == -1) {
-                                    var h = $(item).height();
-                                    var w = $(item).width();
-                                    $(item).css('width', w + 'px').css('height', h + 'px');
-                                    $(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]);
-                                }
-                            });
-                        }
-                    }
-                });
-
-                //Keep replacing
-                if (time > 0)
-                    setTimeout(function () { self.handleImages(lstImgs, time); }, time);
+                    });
+                }
             }
-        });
+        }
+        
+        const replaceVideos = (lstVideos, item) => {
+            if ($.inArray($(item).attr('src'), lstVideos) == -1) {
+                var h = $(item).height();
+                var w = $(item).width();
+        
+                //If image loaded
+                if (h > 0 && w > 0) {
+                    //Replace
+                    $(item).css('width', w + 'px').css('height', h + 'px');
+                    $(item).attr('src', lstVideos[Math.floor(Math.random() * lstVideos.length)]);
+                    $(item).attr('poster', lstVideos[Math.floor(Math.random() * lstVideos.length)]);
+        
+                } else {
+                    //Replace when loaded
+                    $(item).load(function () {
+                        //Prevent 'infinite' loop
+                        if ($.inArray($(item).attr('src'), lstVideos) == -1) {
+                            var h = $(item).height();
+                            var w = $(item).width();
+                            $(item).css('width', w + 'px').css('height', h + 'px');
+                            $(item).attr('src', lstVideos[Math.floor(Math.random() * lstVideos.length)]);
+                            $(item).attr('poster', lstVideos[Math.floor(Math.random() * lstVideos.length)]);
+        
+                        }
+                    });
+                }
+            }
+        }
+        
+        const handleImages = (images, time) => {
+            setInterval(() => {
+        
+                $.each($('img'), (i, item) => {
+                    //Skip if image is already replaced
+                    replaceImages(images, item)
+                });
+                $.each($('video'), (i, item) => {
+                    //Skip if image is already replaced
+                    replaceVideos(images, item)
+                });
+            }, time)
+        }
+
+        const fourMinutesImgs = [
+            'https://img.r7.com/images/croacia-x-brasil-copa-do-mundo-09122022145122740?dimensions=771x420&no_crop=true',
+            'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt7d847935bd3ad1ed/639351f602b9015c9dff3421/GettyImages-1447951636.jpg',
+            'https://img.r7.com/images/croacia-x-brasil-copa-do-mundo-09122022145013908?dimensions=771x420&&amp;&amp;resize=771x420&amp;crop=1280x697+0+101&amp;&amp;resize=771x420&amp;crop=1280x697+0+101',
+            'https://www.oitomeia.com.br/wp-content/uploads/2022/12/Brasil-4.jpg',
+            'https://s2.glbimg.com/-VvRCkB2g6Mhd-Cl9M0CQgMXrEg=/0x0:1599x897/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/B/w/WZCfQwTUeRjfMEjP871Q/gol8j.jpg',
+            'https://f.i.uol.com.br/fotografia/2022/12/10/16706922006394bd685b1fd_1670692200_3x2_md.jpg',
+            'https://www.otempo.com.br/image/contentid/policy:1.2780617:1670703082/croacia-x-brasil-jpg.jpg',
+            'https://www.redebrasilatual.com.br/wp-content/uploads/2022/12/BrasilxCroacia_Fifa.jpg',
+            'https://static.dw.com/image/64051338_1006.jpg',
+            'http://m.lance.com.br/files/admin_slider_thumbnail/uploads/2022/12/09/6393768019a14.jpeg',
+            'https://s02.video.glbimg.com/x720/11192289.jpg'
+        ]
 
         //Run on jQuery ready
         $(function () {
-            self.handleImages(self.fourMinutesImgs, 3000);
+            handleImages(fourMinutesImgs, 3000);
         });
     };
 
